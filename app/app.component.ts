06a01c23b848bbdb36d1fb94134a82fd58d6729d
Priya Choudhary <priya@tothenew.com>
2016-05-19 23:56:34 +0530

    Dummy App

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'app',
+  templateUrl:'app/app.component.html'
+})
+
+export class AppComponent {
+  title = 'Angular2 Demo';
+}
+
